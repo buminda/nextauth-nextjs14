d49909f7f8ed018960164d9539b115b0a61fb63e
Buminda Nawagamuwa <buminda.nawagamuwa@health.telstra.com>
2024-06-17 08:30:52 +1000

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SessionWrapper from "../../components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body className={inter.className}>
+          {/* <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider> */}
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
